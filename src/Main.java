b8ccaadbfdf27846ff90b9e7f3647008e0dc0baa
Pasha V <yourwaof@gmail.com>
2019-09-14 14:50:44 +0300

    Main written

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import com.skillbox.airport.*;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calendar currentCalendar = new GregorianCalendar();
+        Date date = currentCalendar.getTime();
+        currentCalendar.add(Calendar.HOUR, 2);
+        Date currentDate = currentCalendar.getTime();
+
+
+        Airport svo = Airport.getInstance();
+        //System.out.println(svo.getAllAircrafts());
+        //System.out.println(svo.getTerminals());
+
+        svo.getTerminals().stream().forEach(e1 -> e1.getFlights().stream()
+                .filter(o1 -> o1.getDate().after(date) && o1.getDate().before(currentDate) && o1.getType().equals(Flight.Type.DEPARTURE))
+                .forEach(e2 -> System.out.println(e2.getAircraft() + " " + e2.getDate().getHours() + ":" + e2.getDate().getMinutes())));
+
+
+    }
+}
